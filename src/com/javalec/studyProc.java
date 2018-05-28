a5cabd6de4eb38c67c0966645dcca28cecbb7fe7
Bae Sungho <bae9808.gmail.com>
2018-05-28 19:10:22 +0900

    java proc

---
+++ src/com/javalec/studyProc.java
@@ -0,0 +1,29 @@
+package com.javalec;
+
+import java.util.Scanner;
+
+public class studyProc {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int input_ = scanner.nextInt();
+		
+		studyProc sp = new studyProc();
+		
+		sp.sum_(input_);
+		
+	}
+	
+	
+	public void sum_(int i) {
+		
+		for (int j=1; j < 10; j++) {
+			
+			System.out.println(i+ " * "+j+" = "+(i*j));
+			
+		} 
+	}
+	
+	
+}
