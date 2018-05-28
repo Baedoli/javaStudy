3727a5d10975a7df46ee7d3bd87bcc3134a9d6ee
Bae Sungho <bae9808.gmail.com>
2018-05-28 18:53:13 +0900

    java study arr add..

---
+++ src/com/javalec/studyArr.java
@@ -0,0 +1,26 @@
+package com.javalec;
+
+public class studyArr {
+
+	public static void main(String[] args) {
+		
+	  int[] iArr1 = {10,20,30,40,50};
+	  
+	  System.out.println(iArr1[3]);
+	  
+	  int[] iArr2 = new int[3];
+	  
+	  iArr2[0] = 10;
+	  iArr2[1] = 20;
+	  iArr2[2] = 30;
+	  
+	  for (int i = 0; i < iArr2.length; i++) {
+		
+		  System.out.println(iArr2[i]);
+		  
+	}
+		
+	}
+	
+	
+}
