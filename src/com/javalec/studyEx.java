ff03724eac3b9a4e43f81ac760d3818932fc7fa7
Bae Sungho <bae9808.gmail.com>
2018-05-28 16:05:04 +0900

    javastrudy first
    firstcommit

---
+++ src/com/javalec/studyEx.java
@@ -0,0 +1,14 @@
+package com.javalec;
+
+public class studyEx {
+
+	public static void main(String[] args) {
+		
+		int a_ = 10;
+		int b_ = 20;
+		
+		System.out.println(a_);
+		System.out.println(b_);
+		
+	}
+}
